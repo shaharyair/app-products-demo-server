3d043c1c94767539e3a9c52492128619bc5250ff
Shahar Yair <shaharyair12@gmail.com>
2023-11-15 17:54:16 +0200

    controller: Create getProductById function
    Taking the product Id from the request params if product Id exists using the product model it find the product and returns it to the user, if product is not found an error is being thrown

--- controllers/productController.js
+++ controllers/productController.js
@@ -10,6 +10,26 @@ exports.getAllProducts = async (req, res, next) => {
   }
 };
 
+exports.getProductById = async (req, res, next) => {
+  try {
+    const { productId } = req.params;
+
+    if (!productId) {
+      return next(new AppError(`Please provide a valid product id.`, 404));
+    }
+
+    const productById = await Product.findById(productId);
+
+    if (!productById) {
+      return next(new AppError(`Product with id ${productId} was not found`, 404));
+    }
+
+    res.status(200).json({ productById });
+  } catch (err) {
+    next(err);
+  }
+};
+
 exports.addNewEmptyProduct = async (req, res, next) => {
   try {
     const newProduct = new Product({
@@ -30,6 +50,10 @@ exports.addNewEmptyProduct = async (req, res, next) => {
 exports.updateProductInfo = async (req, res, next) => {
   try {
     const { productId } = req.params;
+
+    if (!productId) {
+      return next(new AppError(`Please provide a valid product id.`, 404));
+    }
 
     const updatedProductInfo = await Product.findByIdAndUpdate(productId, req.body, { new: true, runValidators: true });
 
